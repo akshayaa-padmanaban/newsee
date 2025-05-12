e74c02660730da14900c9e083c3084af95b09976
akshayaapadmanaban@gmail.com <akshayaapadmanaban@gmail.com>
2025-05-12 18:58:53 +0530

    feat : search funtion inside dropdown was added

--- lib/pages/kyc_page.dart
+++ lib/pages/kyc_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reactive_forms/reactive_forms.dart';
+import 'package:flutter/services.dart';
 
 class KycPage extends StatelessWidget {
   final String title;
@@ -11,34 +12,51 @@ class KycPage extends StatelessWidget {
     'panno': FormControl<String>(validators: [Validators.required]),
     'aadhaarno': FormControl<String>(validators: [Validators.required]),
     'otheridproof': FormControl<String>(validators: [Validators.required]),
+    'otheridno': FormControl<String>(validators: [Validators.required]),
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("KYC"),
-       ),
+      appBar: AppBar(
+        title: Text("KYC"),
+      ),
       body: ReactiveForm(
         formGroup: form,
         child: SingleChildScrollView(
           child: Column(
             children: [
-              _buildDropdown(
+              Dropdown(
                 controlName: 'applicanttype',
                 label: 'Applicant Type',
                 items: ['MSME', 'Retail'],
               ),
-              _buildTextField('panno', 'PAN No'),
-              _buildTextField('aadhaarno', 'Aadhaar No'),
-              _buildDropdown(
+              TextField('panno', 'PAN No'),
+              IntegerTextField('aadhaarno', 'Aadhaar No'),
+              Dropdown(
                 controlName: 'otheridproof',
                 label: 'Other ID Proof',
-                items: ['Direct Call', 'Lead Management System', 'Online', 'Partner', 'Website'],
+                items: ['Driving License', 'Nrega Card'],
+              ),
+              ReactiveValueListenableBuilder<String>(
+                formControlName: 'otheridproof',
+                builder: (context, control, child) {
+                  return control.value != null && control.value!.isNotEmpty
+                  ? IntegerTextField('otheridno', 'Other ID No')
+                  : SizedBox.shrink();
+                  },
               ),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
+                    if (form.valid) {
+                      final tabController = DefaultTabController.of(context);
+                      if (tabController.index < tabController.length - 1) {
+                        tabController.animateTo(tabController.index + 1);
+                      }
+                    } else {
+                      form.markAllAsTouched();
+                    }
                   },
                   child: Text('Next'),
                 ),
@@ -50,7 +68,7 @@ class KycPage extends StatelessWidget {
     );
   }
 
-  Widget _buildDropdown({
+  Widget Dropdown({
     required String controlName,
     required String label,
     required List<String> items,
@@ -72,20 +90,32 @@ class KycPage extends StatelessWidget {
       ),
     );
   }
-}
 
-  Widget _buildTextField(String controlName, String label) {
+  Widget IntegerTextField(String controlName, String label) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: ReactiveTextField<String>(
         formControlName: controlName,
-        //  keyboardType: controlName == 'mobilenumber' ? TextInputType.number : TextInputType.text,
-        //  inputFormatters: controlName == 'mobilenumber'
-        //   ? [FilteringTextInputFormatter.digitsOnly]
-        //   : [],
+        keyboardType: TextInputType.number, 
+        inputFormatters: [
+          FilteringTextInputFormatter.digitsOnly, 
+        ],
         decoration: InputDecoration(
           labelText: label,
         ),
       ),
     );
   }
+
+  Widget TextField(String controlName, String label) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ReactiveTextField<String>(
+        formControlName: controlName,
+        decoration: InputDecoration(
+          labelText: label,
+        ),
+      ),
+    );
+  }
+}
